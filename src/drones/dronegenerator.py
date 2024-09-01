de3aea66d6a127289ba00d5a153a7d63d1f986dd
V <V@DESKTOP-2KS75VT>
2024-09-01 02:30:17 +0100

    Drone generator refactored

--- src/drones/dronegenerator.py
+++ src/drones/dronegenerator.py
@@ -1,35 +1,37 @@
 from cityMap.citymap import Coordinate
-from typing import List
 from common.decorators import auto_str
 from drones.drone import Drone
+from typing import List
 import copy
+
 
 
 @auto_str
 class DroneGenerator:
-    """Drone generator: generate drones"""
-    
     def __init__(self, warehouses: List[Coordinate]):
         self.warehouses = warehouses
-        self.warehouse_pointer = 0
-        self.ids = 0
-    
-    def get_drones(self, num) -> List[Drone]:
-        """
-        Create and initialize a list of Drone instances.
+        self._warehouse_index = 0
+        self._next_drone_id = 1
 
-        The drone will be initialized to a warehouse. The strategy for determining which warehouse to be initialized
-        in is Round Robin.
-        The warehouse pointer will increment by 1 when a new drone is generated.
-
-        :return: a list of new Drone instances
-        """
-        drones = list()
-        for i in range(num):
-            self.ids += 1
-            start_location = copy.deepcopy(self.warehouses[self.warehouse_pointer])
-            drone = Drone(drone_id=self.ids, warehouses=self.warehouses,
-                          start_location=start_location, height=0)
-            self.warehouse_pointer = (self.warehouse_pointer + 1) % len(self.warehouses)
+    def generate_drones(self, quantity: int) -> List[Drone]:
+        drones = []
+        for _ in range(quantity):
+            drone = self._create_drone()
             drones.append(drone)
+            self._advance_warehouse_pointer()
         return drones
+
+    def _create_drone(self) -> Drone:
+        start_location = copy.deepcopy(self.warehouses[self._warehouse_index])
+        drone = Drone(
+            drone_id=self._next_drone_id,
+            warehouses=self.warehouses,
+            start_location=start_location,
+            height=0
+        )
+        self._next_drone_id += 1
+        return drone
+
+    def _advance_warehouse_pointer(self):
+        """Advance the warehouse pointer to the next warehouse in a Round Robin manner."""
+        self._warehouse_index = (self._warehouse_index + 1) % len(self.warehouses)
