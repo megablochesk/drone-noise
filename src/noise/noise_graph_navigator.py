19acc261f18058baa5c6b88d82eb2b8bc3fb1820
Volodymyr Shevchenko <vladimir_337@ukr.net>
2025-07-29 23:13:32 +0100

    Add warehouse location caching

--- src/noise/noise_graph_navigator.py
+++ src/noise/noise_graph_navigator.py
@@ -3,23 +3,35 @@ from scipy.spatial import cKDTree
 
 from noise.noise_graph_builder import load_or_build_graph
 from common.coordinate import Coordinate
+from common.configuration import LONDON_WAREHOUSES
 
 
 class NoiseGraphNavigator:
     def __init__(self):
         self.graph = load_or_build_graph()
-
-        self.tree = self.build_kdtree()
         self.nodes = list(self.graph.nodes)
+        self.tree = self.build_kdtree()
+
+        self._warehouse_node_cache = {
+            coord: self._query_kdtree(coord)
+            for _, coord in LONDON_WAREHOUSES
+        }
 
     def build_kdtree(self):
-        return cKDTree(
-            [(self.graph.nodes[node]["easting"], self.graph.nodes[node]["northing"]) for node in self.graph]
-        )
+        return cKDTree([
+            (self.graph.nodes[node]["easting"], self.graph.nodes[node]["northing"])
+            for node in self.graph
+        ])
 
-    def find_nearest_node(self, point: Coordinate) -> tuple:
+    def _query_kdtree(self, point: Coordinate) -> tuple:
         _, index = self.tree.query((point.easting, point.northing))
         return self.nodes[index]
+
+    def find_nearest_node(self, point: Coordinate) -> tuple:
+        cached = self._warehouse_node_cache.get(point)
+        if cached:
+            return cached
+        return self._query_kdtree(point)
 
     def get_optimal_route(self, start: Coordinate, end: Coordinate):
         start_node = self.find_nearest_node(start)
