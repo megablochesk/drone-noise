416245b7b9ab47bf7bc813714c0ab52400396f6d
Volodymyr Shevchenko <54890142+megablochesk@users.noreply.github.com>
2026-03-02 03:35:54 +0000

    Change default simulation configs

--- src/common/simulation_configs.py
+++ src/common/simulation_configs.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass, replace
 
 from common.enum import NavigationType
-from common.path_configs import ORDER_BASE_PATH_MIXED_50_50
+from common.path_configs import ORDER_BASE_PATH_RANDOM
 
 
 @dataclass(frozen=True)
@@ -12,14 +12,14 @@ class SimulationConfig:
     print_model_stats: bool = True
 
     orders_in_dataset: int = 100_000
-    orders_to_process: int = 100
-    number_of_drones: int = 100
+    orders_to_process: int = 100_000
+    number_of_drones: int = 1250
 
-    drone_landing: bool = False
+    drone_landing: bool = True
 
     navigator_type: NavigationType = NavigationType.STRAIGHT
 
-    order_dataset_path: str = ORDER_BASE_PATH_MIXED_50_50
+    order_dataset_path: str = ORDER_BASE_PATH_RANDOM
 
     def with_overrides(self, **kwargs) -> "SimulationConfig":
         return replace(self, **kwargs)
